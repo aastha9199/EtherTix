3d264aeaaf21e952590a29bbd1287f0fe0138e0f
Aastha Agarwal <109139737+aastha9199@users.noreply.github.com>
2023-08-22 01:31:59 +0530

    Basic smart contract added for NFT

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract TokenMaster {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract TokenMaster is ERC721 {
+    address public owner;
+    
+    constructor (
+        string memory _name, 
+        string memory _symbol) 
+    ERC721(_name, _symbol) {
+        owner = msg.sender;
+    }
+}
--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -1,5 +1,33 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
+
+const NAME = "TokenMaster"
+const SYMBOL = "TM"
 
 describe("TokenMaster", () => {
 
+    let TokenMaster
+    let deployer, buyer
+
+    beforeEach(async() => {
+
+        [deployer, buyer] = await ethers.getSigners()
+
+        const TokenMaster = await ethers.getContractFactory("TokenMaster")
+        tokenMaster = await TokenMaster.deploy(NAME, SYMBOL)
+    })
+
+    describe("Deployment", () => {
+        it("Sets the name", async() => {
+            expect(await tokenMaster.name()).to.equal(NAME)
+        })
+
+        it("Sets the symbol", async() => {
+            expect(await tokenMaster.symbol()).to.equal(SYMBOL)
+        })
+
+        it("Sets the owner", async() => {
+            expect(await tokenMaster.owner()).to.equal(deployer.address)
+        })
+    })
 })
