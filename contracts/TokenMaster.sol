fa6bc6dc3b29fc6bb481cf59bf588e2a7bb4b683
Aastha Agarwal <109139737+aastha9199@users.noreply.github.com>
2023-08-22 08:07:48 +0530

    smart contract added for events and to buy tickets

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -6,10 +6,83 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract TokenMaster is ERC721 {
     address public owner;
     
+    uint256 public totalOccasions;
+    uint256 public totalSupply;
+
+    struct Occasion {
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+
+    mapping(uint256 => Occasion) occasions;
+    mapping(uint256 => mapping(address => bool)) public hasBought;
+    mapping(uint256 => mapping(uint256 => address)) public seatTaken;
+    mapping(uint256 => uint256[]) seatsTaken;
+
+        modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
     constructor (
         string memory _name, 
         string memory _symbol) 
     ERC721(_name, _symbol) {
         owner = msg.sender;
+    }
+
+    function list(
+        string memory _name,
+        uint256 _cost,
+        uint256 _maxTickets,
+        string memory _date,
+        string memory _time,
+        string memory _location
+    ) public onlyOwner{
+        totalOccasions++;
+        occasions[totalOccasions] = Occasion(
+            totalOccasions,
+            _name,
+            _cost,
+            _maxTickets,
+            _maxTickets,
+            _date,
+            _time,
+            _location
+        );
+    }
+
+    function mint(uint256 _id, uint256 _seat) public payable {
+      
+        require(_id != 0);
+        require(_id <= totalOccasions);
+        require(msg.value >= occasions[_id].cost);
+        require(seatTaken[_id][_seat] == address(0));
+        require(_seat <= occasions[_id].maxTickets);
+
+        occasions[_id].tickets -= 1;
+
+        hasBought[_id][msg.sender] = true; 
+        seatTaken[_id][_seat] = msg.sender;
+
+        seatsTaken[_id].push(_seat); 
+
+        totalSupply++;
+
+        _safeMint(msg.sender, totalSupply);
+    }
+
+    function getOccasion(uint256 _id) public view returns (Occasion memory) {
+        return occasions[_id];
+    }
+
+    function getSeatsTaken(uint256 _id) public view returns (uint256[] memory) {
+        return seatsTaken[_id];
     }
 }
--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -4,6 +4,13 @@ const { ethers } = require("hardhat")
 const NAME = "TokenMaster"
 const SYMBOL = "TM"
 
+const OCCASION_NAME = "ETH Texas"
+const OCCASION_COST = ethers.utils.parseUnits('1', 'ether')
+const OCCASION_MAX_TICKETS = 100
+const OCCASION_DATE = "Apr 27"
+const OCCASION_TIME = "10:00AM CST"
+const OCCASION_LOCATION = "Austin, Texas"
+
 describe("TokenMaster", () => {
 
     let TokenMaster
@@ -15,6 +22,16 @@ describe("TokenMaster", () => {
 
         const TokenMaster = await ethers.getContractFactory("TokenMaster")
         tokenMaster = await TokenMaster.deploy(NAME, SYMBOL)
+
+        const transaction = await tokenMaster.connect(deployer).list(
+            OCCASION_NAME,
+            OCCASION_COST,
+            OCCASION_MAX_TICKETS,
+            OCCASION_DATE,
+            OCCASION_TIME,
+            OCCASION_LOCATION
+        )
+        await transaction.wait()
     })
 
     describe("Deployment", () => {
@@ -30,4 +47,59 @@ describe("TokenMaster", () => {
             expect(await tokenMaster.owner()).to.equal(deployer.address)
         })
     })
+
+    describe("Occasions", () => {
+        it('Updates occasions count', async() => {
+            const totalOccasions = await tokenMaster.totalOccasions()
+            expect(totalOccasions).to.be.equal(1)
+        })
+        it('Returns occasions attributes', async () => {
+            const occasion = await tokenMaster.getOccasion(1)
+            expect(occasion.id).to.be.equal(1)
+            expect(occasion.name).to.be.equal(OCCASION_NAME)
+            expect(occasion.cost).to.be.equal(OCCASION_COST)
+            expect(occasion.tickets).to.be.equal(OCCASION_MAX_TICKETS)
+            expect(occasion.date).to.be.equal(OCCASION_DATE)
+            expect(occasion.time).to.be.equal(OCCASION_TIME)
+            expect(occasion.location).to.be.equal(OCCASION_LOCATION)
+          })
+    })
+    
+    describe("Minting", () => {
+        const ID = 1
+        const SEAT = 50
+        const AMOUNT = ethers.utils.parseUnits('1', 'ether')
+    
+        beforeEach(async () => {
+          const transaction = await tokenMaster.connect(buyer).mint(ID, SEAT, { value: AMOUNT })
+          await transaction.wait()
+        })
+    
+        it('Updates ticket count', async () => {
+          const occasion = await tokenMaster.getOccasion(1)
+          expect(occasion.tickets).to.be.equal(OCCASION_MAX_TICKETS - 1)
+        })
+    
+        it('Updates buying status', async () => {
+          const status = await tokenMaster.hasBought(ID, buyer.address)
+          expect(status).to.be.equal(true)
+        })
+    
+        it('Updates seat status', async () => {
+          const owner = await tokenMaster.seatTaken(ID, SEAT)
+          expect(owner).to.equal(buyer.address)
+        })
+    
+        it('Updates overall seating status', async () => {
+          const seats = await tokenMaster.getSeatsTaken(ID)
+          expect(seats.length).to.equal(1)
+          expect(seats[0]).to.equal(SEAT)
+        })
+    
+        it('Updates the contract balance', async () => {
+          const balance = await ethers.provider.getBalance(tokenMaster.address)
+          expect(balance).to.be.equal(AMOUNT)
+        })
+      })
+    
 })
